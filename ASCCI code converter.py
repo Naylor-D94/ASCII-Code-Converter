a7625b22cab5440f3563fbecf55cbe03b901244e
Naylor-D94 <naylor-2010@hotmail.co.uk>
2021-12-06 19:58:47 +0000

    First Commit

---
+++ ASCCI code converter.py
@@ -0,0 +1,12 @@
+alpha=input("Enter any alphabet:")
+
+if ord(alpha)>=65 and ord(alpha)<=90:
+    print("You entered an uppercase")
+
+if ord(alpha)>=97 and ord(alpha)<=122:
+    print("You entered a lowercase")
+
+if ord(alpha)>48 and ord(alpha)<=57:
+    print("you entered a number")
+
+
